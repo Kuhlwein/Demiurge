c7a957f7c0adef30f1f37b1400a287afd2ccdf8a
Morten Kuhlwein <morten@kuhlwein.dk>
2020-07-18 15:40:55 +0200

    hillshade

--- src/appearance/Hillshade.cpp
+++ src/appearance/Hillshade.cpp
@@ -3,46 +3,75 @@
 //
 
 #include <Shader.h>
+#include <imgui/imgui.h>
 #include "Hillshade.h"
+#include "Project.h"
 
 Hillshade::Hillshade() : Appearance("Hillshade") {
+	hillshade_texture = new Texture(100, 1, GL_RGBA, "gradient_hillshade_"+sid, GL_LINEAR);
+	gradient = new GradientMenu("Hillshade gradient");
+	gradient->setGradient(std::vector<int>{0, 0, 0, 255, 255, 255});
+	gradient->toTexture(hillshade_texture);
 	shader = Shader::builder()
 			.include(fragmentColor)
 			.include(def_pi)
-			.include(texturespace_gradient)
-			.create(R"(
-)",R"(
+			.include(get_slope)
+			.include(get_aspect)
+			.create(replaceSID(R"(
+uniform float z_factor_SID;
+uniform float zenith_SID;
+uniform float azimuth_SID;
+uniform sampler2D gradient_hillshade_SID;
+)"),replaceSID(R"(
 
-vec3 normalv = vec3(delta_x,delta_y,0.1);
-normalv = (normalv/length(normalv)+1)/2;
+float slope =  get_slope(z_factor_SID);
+float aspect = get_aspect();
 
-float z_factor = 5.0f;
-float slope =  atan(z_factor * sqrt(pow(delta_x,2) + pow(delta_y,2)));
-
-float aspect = atan(delta_y, -(delta_x));
-aspect=aspect+M_PI;
-
-float altitude = M_PI/4;
-float zenith = M_PI/2-altitude;
-
-float azimuth = 315;
-float azimuth_math = (azimuth - 90)/180*M_PI;
-
-float hillshade = ((cos(zenith) * cos(slope)) +
-             (sin(zenith) * sin(slope) * cos(azimuth_math - aspect)));
+float hillshade = ((cos(zenith_SID) * cos(slope)) + (sin(zenith_SID) * sin(slope) * cos(-azimuth_SID + M_PI/2 - aspect)));
 
 
-//fc = vec4(normalv,0);
-//fc = vec4(hillshade,hillshade,hillshade,0);
-//fc = vec4(slope,slope,slope,0);
-//fc = vec4(aspect,aspect,aspect,0);
-vec4 k = texture(gradient_ocean_0,vec2(hillshade,0));
+vec4 k = texture(gradient_hillshade_SID,vec2(hillshade,0));
 fc = fc*(1-k.a) + k*(k.a);
-)");
+)"));
 }
 
 bool Hillshade::update_self(Project *p) {
-	return true;
+	if (first) first = false;
+
+	ImGui::DragFloat("Z-factor",&zfactor);
+	ImGui::DragFloat("Altitude",&altitude,1.0,0,90);
+	ImGui::DragFloat("Azimuth",&azimuth,1.0,0,360);
+	ImGui::Spacing();
+	ImGui::Separator();
+	ImGui::Spacing();
+
+	gradient->update();
+
+
+	int current = -1;
+	const char* items[] = { "Grayscale","Green-yellow-red"};
+	if(ImGui::Combo("Preset",&current,items,IM_ARRAYSIZE(items))) {
+		switch (current) {
+			case 0:
+				gradient->setGradient(std::vector<int>{0, 0, 0, 255, 255, 255});
+				break;
+			case 1:
+				gradient->setGradient(
+						std::vector<int>{31, 70, 41, 111, 165, 67, 243, 236, 34, 246, 145, 29, 212, 50, 37});
+				break;
+		}
+	}
+
+	if (ImGui::Button("Apply")) {
+		first = true;
+		return true;
+	}
+	ImGui::SameLine();
+	if (ImGui::Button("Cancel")) {
+		first = true;
+		return true;
+	}
+	return false;
 }
 
 Shader *Hillshade::getShader() {
@@ -50,9 +79,16 @@ Shader *Hillshade::getShader() {
 }
 
 void Hillshade::unprepare(Project *p) {
-
+	p->remove_texture(hillshade_texture);
 }
 
 void Hillshade::prepare(Project *p) {
-
+	gradient->toTexture(hillshade_texture);
+	p->add_texture(hillshade_texture);
+	int id = glGetUniformLocation(p->program->getId(),replaceSID("z_factor_SID").c_str());
+	glUniform1f(id,zfactor);
+	id = glGetUniformLocation(p->program->getId(),replaceSID("azimuth_SID").c_str());
+	glUniform1f(id,azimuth/180*M_PI);
+	id = glGetUniformLocation(p->program->getId(),replaceSID("zenith_SID").c_str());
+	glUniform1f(id,(90-altitude)/180*M_PI);
 }
--- src/appearance/Hillshade.h
+++ src/appearance/Hillshade.h
@@ -5,8 +5,9 @@
 #ifndef DEMIURGE_HILLSHADE_H
 #define DEMIURGE_HILLSHADE_H
 
-
+class Texture;
 #include "Appearance.h"
+#include "GradientMenu.h"
 
 class Hillshade : public Appearance {
 public:
@@ -15,9 +16,15 @@ public:
 	void unprepare(Project* p) override;
 	Shader* getShader() override;
 private:
+	bool first = true;
 	bool update_self(Project* p) override;
 
 	Shader* shader;
+
+	float zfactor=1.0, altitude=45, azimuth=315;
+
+	Texture* hillshade_texture;
+	GradientMenu* gradient;
 };
 
 
